fc6b0227ebca533dc2331a44834f5c4979c68ff3
qhdgktjr <b1217hs@naver.com>
2021-01-14 12:47:11 +0900

    Header / Cookie

--- WebContent/WEB-INF/views/ch05/content.jsp
+++ WebContent/WEB-INF/views/ch05/content.jsp
@@ -25,7 +25,7 @@
 					<div class="sector">
 						<h5>헤더값 읽기</h5>
 							<div>
-								<a class="btn btn-info btn-sm" href="method1">브라우저 정보 얻기</a>
+								<a class="btn btn-info btn-sm" href="method2">브라우저 정보 얻기</a>
 							</div>
 					</div>
 				<!-- 섹터 구분 -->
--- src/main/java/com/mycompany/webapp/controller/Ch05Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch05Controller.java
@@ -1,5 +1,7 @@
 package com.mycompany.webapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -20,10 +22,30 @@ public class Ch05Controller {
 	}
 
 	@GetMapping("/method1")
-	/* 헤더명의 값을 저장 */
+	/* 헤더명의 값을 저장	스프링이 자동으로 객체의내용을 찾아준다. */
 	public String method1(@RequestHeader("User-Agent") String userAgent) {
 		logger.info("실행");
 		/* logger.info(userAgent); */
+		if (userAgent.contains("Edg")) {
+			logger.info("브라우저의 종류 : 엣지 ");
+		} else if (userAgent.contains("Chrome")) {
+			logger.info("브라우저의 종류 : 크롬");
+		} else if (userAgent.contains("Trident/7.0")) {
+			logger.info("브라우저의 종류 : IE11");
+		} else if (userAgent.contains("MSIE")) {
+			logger.info("브라우저의 종류 : IE이하");
+		}
+		return "ch05/content";
+
+	}
+	
+	@GetMapping("/method2")
+						//WAS가 제공하는 요청 저장 객체
+	public String method2(HttpServletRequest request) {
+		logger.info("실행");
+			 String userAgent = request.getHeader("User-Agent");
+		
+		
 		if (userAgent.contains("Edg")) {
 			logger.info("브라우저의 종류 : 엣지 ");
 		} else if (userAgent.contains("Chrome")) {
