8d7c0d822c26c9e7e1246c3edd0df53c19737343
qhdgktjr <b1217hs@naver.com>
2021-01-14 10:28:35 +0900

    param 작업

--- WebContent/WEB-INF/views/ch03/content.jsp
+++ WebContent/WEB-INF/views/ch03/content.jsp
@@ -23,12 +23,36 @@
 				
 				<div class="content">
 					<div class="sector">
-						<h5></h5>
+						<h5>GET 방식 데이터 전달</h5>
 							<div>
-								
+								<a class="btn btn-info btn-sm" href="method1?param1=문자열&param2=5&param3=3.14&param4=true&param5=2021-01-14">데이터 전달</a>
 							</div>
 					</div>
 					
+					<div class="sector">
+						<h5>POST 방식 데이터 전달</h5>
+							<div>
+								<form method="post" action="method1">
+									<input type="text" name="param1" value="문자열"/><br/>
+									<input type="number" name="param2" value="5"/><br/>
+									<input type="number" name="param3" value="3.14"/><br/>
+									<input type="checkbox" name="param4" value="checked"/><br/>
+									<input type="date" name="param5" value="2021-12-25"/><br/>
+									<button class="btn btn-info btn-sm">데이터 전달</button>
+								</form>
+							</div>
+					</div>
+					
+					<div class="sector">
+						<h5>디폴트 값</h5>
+							<div>
+								<form method="post" action="method3">
+									<input type="text" name="param1" value="good"/><br/>
+									<input type="text" name="param2" value="5"/><br/>
+									<button class="btn btn-info btn-sm">데이터 전달</button>
+								</form>
+							</div>
+					</div>
 				
 				
 				
--- src/main/java/com/mycompany/webapp/controller/ch03Controller.java
+++ src/main/java/com/mycompany/webapp/controller/ch03Controller.java
@@ -1,10 +1,14 @@
 package com.mycompany.webapp.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/ch03")
@@ -17,4 +21,53 @@ public class ch03Controller {
 		logger.info("실행");
 		return "ch03/content";
 	}
+	
+	@RequestMapping("/method1")
+	public String method1(
+			/* 기본으로 String이고, 자동으로 타입을 변환시켜준다 , Date는 기본패턴을 지정해줘야한다.*/
+			String param1, int param2, double param3, boolean param4, 
+			@DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {
+		logger.info("param1 : " + param1);
+		logger.info("param1 : " + param2);
+		logger.info("param1 : " + param3);
+		logger.info("param1 : " + param4);
+		logger.info("param1 : " + param5);
+		
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method2")
+	public String method2(
+			/* 기본으로 String이고, 자동으로 타입을 변환시켜준다 , Date는 기본패턴을 지정해줘야한다.*/
+			@RequestParam("param1") String arg1, 
+			@RequestParam("param2") int arg2,
+			double param3,
+			boolean param4, 
+			@DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {
+		logger.info("param1 : " + arg1);
+		logger.info("param1 : " + arg2);
+		logger.info("param1 : " + param3);
+		logger.info("param1 : " + param4);
+		logger.info("param1 : " + param5);
+		
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method3")
+	/* http 500 서버 에러 
+	 *  변수 값이 없을 때만 적용, 값이 있으면 입력 값으로 보여준다.
+	 * 	기본타입이 String여서 null을 다른타입값으로 변환이 안되어 에러*/
+	public String method3(
+			 String param1, 
+			 @RequestParam(defaultValue="0") int param2,
+			 @RequestParam(defaultValue="0.0") double param3,
+			 @RequestParam(defaultValue="0") boolean param4) {
+		logger.info("param1 : " + param1);
+		logger.info("param1 : " + param2);
+		logger.info("param1 : " + param3);
+		logger.info("param1 : " + param4);
+		
+		return "ch03/content";
+	}
+	
 }
